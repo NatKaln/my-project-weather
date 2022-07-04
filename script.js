35b9c051848830e9d5cb286864197372ff21801b
Nataly <vslshnat@gmail.com>
2022-07-04 16:39:59 +0300

    added correct data forecast and rounded temperature

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ if (month < 10) {
 let displayDate = document.querySelector("#currentDate");
 displayDate.innerHTML = `${year}.${month}.${date}`;
 
+//display correct days with forecast
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 // Forecast
 function displayForecast(response) {
   let forecast = response.data.daily;
@@ -31,15 +40,21 @@ function displayForecast(response) {
       forecastHTML +
       `
       <div class="col-2">
-        <strong>${forecastDay.dt}</strong> <br />
+        <strong>${formatDay(forecastDay.dt)}</strong> <br />
         <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min">${forecastDay.temp.min}° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            forecastDay.temp.min
+          )}° </span>
         </div>
       </div>
   `;
