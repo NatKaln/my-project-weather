db4eb1ddc134d955ea627e674173105bcdae61c4
Nataly <vslshnat@gmail.com>
2022-07-11 15:21:31 +0300

    called the getForecast in function locationButton(position)

--- script.js
+++ script.js
@@ -118,13 +118,13 @@ searchEvent.addEventListener("submit", search);
 
 //forecast for current location
 
-function forecastLocationSearch(coordinates) {
-  let apiKey = "ae257b6f200fc59e9f754b38798d7627";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(getForecast);
-}
-let forecastLocation = document.querySelector("#locationSearch");
-forecastLocation.addEventListener("submit", forecastLocationSearch);
+// function forecastLocationSearch(coordinates) {
+//  let apiKey = "ae257b6f200fc59e9f754b38798d7627";
+//  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+//  axios.get(apiUrl).then(getForecast);
+// }
+// let forecastLocation = document.querySelector("#locationSearch");
+// forecastLocation.addEventListener("submit", forecastLocationSearch);
 
 // Weather
 function showWeather(response) {
@@ -222,6 +222,7 @@ function locationButton(position) {
     dayOfMonth.innerHTML = day;
   }
   axios.get(apiUrl).then(showTemperature);
+  getForecast(position);
 }
 
 function getCurrentPosition(event) {
