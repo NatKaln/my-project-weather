5583b315c3be68c99571609527e977a3cdd8383e
Nataly <vslshnat@gmail.com>
2022-07-11 11:22:18 +0300

    treid to add forecast for location current

--- script.js
+++ script.js
@@ -116,6 +116,16 @@ function searchCity(city) {
 let searchEvent = document.querySelector("#changeCity");
 searchEvent.addEventListener("submit", search);
 
+//forecast for current location
+
+function forecastLocationSearch(coordinates) {
+  let apiKey = "ae257b6f200fc59e9f754b38798d7627";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getForecast);
+}
+let forecastLocation = document.querySelector("#locationSearch");
+forecastLocation.addEventListener("submit", forecastLocationSearch);
+
 // Weather
 function showWeather(response) {
   let city = document.querySelector("#curCity");
